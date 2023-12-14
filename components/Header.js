02d3decbed23f237466bec65c5637cd4d65025f0
Alejo <jubany4@gmail.com>
2023-12-14 11:30:39 -0300

    creacion header

--- components/Header.js
+++ components/Header.js
@@ -4,6 +4,13 @@ export default function Header() {
   return (
     <div>
         <Link href={'/'}>Ecommerce</Link>
+        <nav>
+        <Link href={'/'}>Home</Link>
+        <Link href={'/'}>All Products</Link>
+        <Link href={'/'}>Categories</Link>
+        <Link href={'/'}>Account</Link>
+        <Link href={'/'}>Cart (0)</Link>
+        </nav>
     </div>
   )
 }
