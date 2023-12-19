77b8f3514296b79b61c84edcc40642fbbeeded1d
Alejo <jubany4@gmail.com>
2023-12-19 11:16:18 -0300

    creacion button

--- components/Featured.js
+++ components/Featured.js
@@ -1,46 +1,89 @@
+import Center from "@/components/Center";
 import styled from "styled-components";
-import Center from "./Center";
-
-
+// import Button from "@/components/Button";
+// import ButtonLink from "@/components/ButtonLink";
+// import CartIcon from "@/components/icons/CartIcon";
+import {useContext} from "react";
+// import {CartContext} from "@/components/CartContext";
 
 const Bg = styled.div`
-background-color:#222 ;
-color:#fff;
-padding:50px 0;
+  background-color: #222;
+  color:#fff;
+  padding: 50px 0;
 `;
 const Title = styled.h1`
-margin:0;
-font-weight: normal;`
+  margin:0;
+  font-weight:normal;
+  font-size:1.5rem;
+  @media screen and (min-width: 768px) {
+    font-size:3rem;
+  }
+`;
 const Desc = styled.p`
-color: #aaa;
-font-size:.8rem;
-;`
-const Wraper = styled.div`
-display: grid;
-grid-template-columns: .8fr 0.8fr;
-gap: 60px;
-img{
-  max-width: 100%;
-}
-;`
+  color:#aaa;
+  font-size:.8rem;
+`;
+const ColumnsWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  gap: 40px;
+  img{
+    max-width: 100%;
+    max-height: 200px;
+    display: block;
+    margin: 0 auto;
+  }
+  div:nth-child(1) {
+    order: 2;
+  }
+  @media screen and (min-width: 768px) {
+    grid-template-columns: 1.1fr 0.9fr;
+    div:nth-child(1) {
+      order: 0;
+    }
+    img{
+      max-width: 100%;
+    }
+  }
+`;
+const Column = styled.div`
+  display: flex;
+  align-items: center;
+`;
+const ButtonsWrapper = styled.div`
+  display: flex;
+  gap:10px;
+  margin-top:25px;
+`;
 
-export default function () {
+export default function Featured({product}) {
+  // const {addProduct} = useContext(CartContext);
+  // function addFeaturedToCart() {
+  //   addProduct(product._id);
+  // }
   return (
     <Bg>
-      <Center />
-      <Wraper>
-        <div>
-      <Title>Pro Anywhere</Title>
-      <Desc>
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsumlorem ipsumlorem ipsum
-      </Desc>
-      </div>
-      <div>
-        <img src="https://tse1.mm.bing.net/th?id=OIP.ZoXzPxcoAq_ImyX1O14PFAHaIq&pid=Api&P=0&h=180" alt=""/>
-      </div>
-      </Wraper>
+      <Center>
+        <ColumnsWrapper>
+          <Column>
+            <div>
+              <Title>hola</Title>
+              <Desc>lorem ipsum</Desc>
+              <ButtonsWrapper>
+                {/* <ButtonLink href={'/product/'+product._id} outline={1} white={1}>Read more</ButtonLink>
+                <Button white onClick={addFeaturedToCart}>
+                  <CartIcon />
+                  Add to cart
+                </Button> */}
+              </ButtonsWrapper>
+            </div>
+          </Column>
+          <Column>
+            <img src="https://dawid-next-ecommerce.s3.amazonaws.com/1679151719649.png" alt=""/>
+          </Column>
+        </ColumnsWrapper>
+      </Center>
+
     </Bg>
   );
 }
