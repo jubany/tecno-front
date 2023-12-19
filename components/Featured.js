66df83ec0ffda51031b1cfd0ba248dbe37daa063
Alejo <jubany4@gmail.com>
2023-12-19 10:23:59 -0300

    cambios en index

--- components/Featured.js
+++ components/Featured.js
@@ -15,16 +15,32 @@ const Desc = styled.p`
 color: #aaa;
 font-size:.8rem;
 ;`
+const Wraper = styled.div`
+display: grid;
+grid-template-columns: .8fr 0.8fr;
+gap: 60px;
+img{
+  max-width: 100%;
+}
+;`
+
 export default function () {
   return (
     <Bg>
       <Center />
+      <Wraper>
+        <div>
       <Title>Pro Anywhere</Title>
       <Desc>
         lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
         lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
         lorem ipsum lorem ipsum lorem ipsum lorem ipsumlorem ipsumlorem ipsum
       </Desc>
+      </div>
+      <div>
+        <img src="https://tse1.mm.bing.net/th?id=OIP.ZoXzPxcoAq_ImyX1O14PFAHaIq&pid=Api&P=0&h=180" alt=""/>
+      </div>
+      </Wraper>
     </Bg>
   );
 }
